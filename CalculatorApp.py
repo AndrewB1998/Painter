b3307d8934541923a86db47fbea638f1e7b7e155
Andrew <71883814+AndrewB1998@users.noreply.github.com>
2023-03-27 18:14:29 +0300

    Create CalculatorApp.py
    Project start and class creation

---
+++ CalculatorApp.py
@@ -0,0 +1,15 @@
+from tkinter import *
+#root window
+root = Tk()
+
+#title
+root.title('Pocket Calculator')
+#dimensions
+root.geometry("400x400")
+
+#calculator class
+class Calculator:
+    def __init__(self) -> None:
+        pass
+#execute
+root.mainloop()
