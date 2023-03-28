5279286d20073d23943df8fcbdf7652fd2335a39
Andrew <71883814+AndrewB1998@users.noreply.github.com>
2023-03-28 21:12:41 +0300

    Resizable window and structure

--- CalculatorApp.py
+++ CalculatorApp.py
@@ -3,13 +3,18 @@ import tkinter as tk #import tkinter module
 class Calculator(tk.Frame): #create class Calculator
 #root window
     def __init__(self, master): #create constructor
+        self.appUI(master) #call appUI function
+        
+    def appUI(self, master): #create appUI function
         self.master = master #create master
         master.title("Pocket Calculator") #set title
-        master.geometry("300x300") #set geometry
-        master.resizable(0,0) #set resizable
-        
+        master.geometry("800x800") #set geometry
+        master.resizable(width= None, height= None) #set resizable
 
-#execute
-root = tk.Tk() #create root window
-app = Calculator(root)
-root.mainloop()
+def main(): #create main function
+    root = tk.Tk() #create root window
+    app = Calculator(root)
+    root.mainloop()
+
+if __name__ == "__main__": #call main function
+    main()
