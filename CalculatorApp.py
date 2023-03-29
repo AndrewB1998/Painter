ffb026943a00ee7bcc9284a7d9ec5db91570ce19
Andrew <71883814+AndrewB1998@users.noreply.github.com>
2023-03-29 15:49:21 +0300

    Added canvas and a draw class

--- CalculatorApp.py
+++ CalculatorApp.py
@@ -1,19 +1,27 @@
-import tkinter as tk #import tkinter module
+from tkinter import *   #import tkinter
 
-class Calculator(tk.Frame): #create class Calculator
-#root window
+class Painter(Frame): #create class Painter
     def __init__(self, master): #create constructor
         self.appUI(master) #call appUI function
         
     def appUI(self, master): #create appUI function
         self.master = master #create master
         master.title("Pocket Calculator") #set title
-        master.geometry("800x800") #set geometry
         master.resizable(width= None, height= None) #set resizable
+        canvas_width = 800
+        canvas_height = 600
+        self.canvas = Canvas(master, width=canvas_width, height=canvas_height, bg="white") #create canvas
+        self.canvas.pack(fill=BOTH, expand=YES)
+        self.canvas.bind("<B1-Motion>", self.draw)
+
+    def draw(self, event): #create draw function
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.canvas.create_line(x1, y1, x2, y2, fill="black")    
 
 def main(): #create main function
-    root = tk.Tk() #create root window
-    app = Calculator(root)
+    root = Tk() #create root window
+    app = Painter(root)
     root.mainloop()
 
 if __name__ == "__main__": #call main function
